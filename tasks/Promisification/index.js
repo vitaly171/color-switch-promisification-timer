1275a33960a6318b5e6313170f982483e4ef6da9
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2023-03-13 23:29:23 +0300

    Update index.js

--- tasks/Promisification/index.js
+++ tasks/Promisification/index.js
@@ -9,8 +9,42 @@ const delay = (ms) => {
     }, ms);
   });
 };
-const logger = (time) => console.log(`Resolved after ${time}ms`);
+const logger1 = (time) => console.log(`Resolved after ${time}ms`);
 
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+delay(2000).then(logger1); // Resolved after 2000ms
+delay(1000).then(logger1); // Resolved after 1000ms
+delay(1500).then(logger1); // Resolved after 1500ms
+
+//   Second Task
+//   Rewrite the toggleUserState() without callback function,
+//   and a took only two parameters allUsers and userName and returned a promise.
+
+// **Previous function with callback**
+
+// const toggleUserState = (allUsers, userName, callback) => {
+//   const updatedUsers = allUsers.map((user) =>
+//     user.name === userName ? { ...user, active: !user.active } : user
+//   );
+
+//   callback(updatedUsers);
+// };
+
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((resolve) => {
+    const updatedUsers = allUsers.map((user) =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    );
+    resolve(updatedUsers);
+  });
+};
+const logger2 = (updatedUsers) => console.table(updatedUsers);
+
+toggleUserState(users, "Mango").then(logger2);
+toggleUserState(users, "Lux").then(logger2);
